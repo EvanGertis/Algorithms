a1f4301e9c7808d881234c39127f62346d70469e
EvanGertis <39931491+EvanGertis@users.noreply.github.com>
2019-02-12 10:18:14 -0500

    added some helpful comments

--- C_Algorithms/Sorted_linked_list/proc.cs
+++ C_Algorithms/Sorted_linked_list/proc.cs
@@ -2,18 +2,23 @@ using System;
 using System.Runtime.InteropServices;
 
 namespace SortedList{
-    class Sorted{
 
+    class Sorted{
+            // import main function from proclib.dll
             [System.Runtime.InteropServices.DllImportAttribute(@"proclib.dll", EntryPoint="main", CallingConvention=System.Runtime.InteropServices.CallingConvention.StdCall)]
             public static extern int main();
        
         static void Main(){
 
             Console.WriteLine("Running C program sorted_linked_list");
+
+            // call main from Main() in c#
             main();
             // Keep the console window open in debug mode.
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
+
+            
         }
     }
 }
