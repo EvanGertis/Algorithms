7ac0dc0ff0d647fcc82700d3824502aa35df78ae
EvanGertis <39931491+EvanGertis@users.noreply.github.com>
2019-02-19 04:05:52 -0500

    added base code to floyd

--- C_Algorithms/floyds_algorithm/floyd.c
+++ C_Algorithms/floyds_algorithm/floyd.c
@@ -3,7 +3,13 @@
 
 int main(){
 
-    printf("Hello");
+    struct node *head;
+    head = (struct node *)malloc(sizeof(struct node));
+    head->info = 0;
+    head->link = NULL;
+
+    createList(head);
+    displayList(head);
 
     return 0;
 }
--- C_Algorithms/floyds_algorithm/floyd.h
+++ C_Algorithms/floyds_algorithm/floyd.h
@@ -1,2 +1,65 @@
 #include "stdlib.h"
 #include "stdio.h"
+
+
+// struct node definition, stores data and pointer to next node.
+struct node {
+
+    int info;
+    struct node *link;
+};
+
+// function definitions.
+void createList(struct node *head);
+void insertAtEnd(struct node * head, int data);
+void displayList(struct node* head);
+
+
+// createList prompts the user for number of nodes and data to be inserted.
+void createList(struct node *head){
+    int data, n;
+    
+    printf("Enter the number of nodes: ");
+    scanf("%d", &n);
+
+    // gaurd
+    if(n == 0){
+        return;
+    }
+
+    // get the data for the nodes.
+    for(int i =0; i< n; i++){
+        printf("Enter the data that you would like to insert at node %d: ", i);
+        scanf("%d", &data);
+        insertAtEnd(head, data);
+    }
+}
+
+// insert at end adds a node to the end of the list.
+void insertAtEnd(struct node * head, int data){
+    struct node * temp, * p;
+    temp = (struct node *)malloc(sizeof(struct node));
+    temp->info = data;
+
+    p = head;
+    while(p->link != NULL){
+        p = p->link;
+    }
+
+    temp->link = p->link;
+    p->link = temp;
+
+}
+
+// displayList just prints out the valuse stored in each node.
+void displayList(struct node *head){
+    struct node *p;
+
+    p = head->link;
+    while(p != NULL){
+        printf("|%d | ", p->info);
+        p = p->link;
+    }
+
+
+}
