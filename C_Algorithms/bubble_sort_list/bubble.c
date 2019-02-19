27f3adadda9d3128a9fbd8832f8c8d3bf1244e1d
EvanGertis <39931491+EvanGertis@users.noreply.github.com>
2019-02-19 03:29:05 -0500

    moved definitions and implementation to header

--- C_Algorithms/bubble_sort_list/bubble.c
+++ C_Algorithms/bubble_sort_list/bubble.c
@@ -1,9 +1,6 @@
 #include "bubble.h"
 
 
-void sortByExchangingData(struct node *start);
-struct node *sortByExchangingLinks(struct node *start);
-
 // main entry point.
 int main(){
     struct node * head;
@@ -25,43 +22,3 @@ int main(){
     return 0;
 }
 
-void sortByExchangingData(struct node *start){
-    struct node *end, *p, *q;
-    int temp;
-
-    for(end = NULL; end != start->link; end = p){
-        for(p = start; p->link!=end; p = p->link){
-            q = p->link;
-            if(p->info > q->info){
-                temp = p->info;
-                p->info = q->info;
-                q->info = temp;
-            }
-        }
-    }
-
-}
-
-struct node  *sortByExchangingLinks(struct node *start){
-    struct node *end, *r, *p, *q, *temp;
-    
-    for(end = NULL; end != start->link; end = p){
-        for(r=p=start; p->link!=end; r = p, p=p->link){
-            q = p->link;
-            if(p->info > q->info){
-                p->link = q->link;
-                q->link = p;
-                if(p!=start){
-                    r->link = q;
-                } else {
-                    start = q;
-                }
-                temp = p;
-                p = q;
-                q = temp;
-            }
-        }
-    }
-
-    return start;
-}
--- C_Algorithms/bubble_sort_list/bubble.h
+++ C_Algorithms/bubble_sort_list/bubble.h
@@ -11,6 +11,10 @@ struct node
 void createList(struct node *head);
 void insertAtEnd(struct node *head, int data);
 void displayList(struct node *head);
+void sortByExchangingData(struct node *start);
+struct node *sortByExchangingLinks(struct node *start);
+
+
 
 void createList(struct node *head)
 {
@@ -67,4 +71,45 @@ void displayList(struct node *head)
         p = p->link;
     }
     printf("\n");
+}
+
+void sortByExchangingData(struct node *start){
+    struct node *end, *p, *q;
+    int temp;
+
+    for(end = NULL; end != start->link; end = p){
+        for(p = start; p->link!=end; p = p->link){
+            q = p->link;
+            if(p->info > q->info){
+                temp = p->info;
+                p->info = q->info;
+                q->info = temp;
+            }
+        }
+    }
+
+}
+
+struct node  *sortByExchangingLinks(struct node *start){
+    struct node *end, *r, *p, *q, *temp;
+    
+    for(end = NULL; end != start->link; end = p){
+        for(r=p=start; p->link!=end; r = p, p=p->link){
+            q = p->link;
+            if(p->info > q->info){
+                p->link = q->link;
+                q->link = p;
+                if(p!=start){
+                    r->link = q;
+                } else {
+                    start = q;
+                }
+                temp = p;
+                p = q;
+                q = temp;
+            }
+        }
+    }
+
+    return start;
 }
