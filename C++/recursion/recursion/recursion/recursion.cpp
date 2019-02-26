00f684b8b2a9c930a12d9a2bdc29b646da50918a
EvanGertis <39931491+EvanGertis@users.noreply.github.com>
2019-02-26 05:46:16 -0500

    added head

--- C++/recursion/recursion/recursion/recursion.cpp
+++ C++/recursion/recursion/recursion/recursion.cpp
@@ -4,16 +4,16 @@
 #include "pch.h"
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n"; 
+void tail(int n) {
+	if (n == 0) return;
+
+	printf("Tail function called with n= %d \n", n);
+	
+	// call the function recursively, the stack will not store the previous calls.
+	tail(n - 1);
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu\\\
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
-
+int main()
+{
+	tail(3);
+}
