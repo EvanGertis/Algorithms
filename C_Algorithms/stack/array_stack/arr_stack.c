1905e7169200144f537fe61a120c221de0eee039
EvanGertis <39931491+EvanGertis@users.noreply.github.com>
2019-03-04 03:58:57 -0500

    added loops and cases

--- C_Algorithms/stack/array_stack/arr_stack.c
+++ C_Algorithms/stack/array_stack/arr_stack.c
@@ -26,8 +26,56 @@ int pop();
 void display();
 
 int main(){
+    
+    //locals.
+    int choice, x;
+    initializeStack();
 
-    printf("Hi there.");
+    //interface.
+    while(1){
+        printf("1.Push an element on the stack\n");
+        printf("2.Pop and element from the stack\n");
+        printf("3.Display the top element\n");
+        printf("4.Display all stack elements\n");
+        printf("5.Display the size of the stack\n");
+        printf("6. Quit\n");
+        printf("Enter you choice :\n");
+        printf("%d", &choice);
+    }
+    
+    //parse the selection.
+    switch (choice)
+    {
+        case 1:
+            printf("Please enter the number that you want to push\n");
+            scanf("%d", &x);
+            push(x);
+            break;
+    
+        case 2:
+            /* code */
+            break;
+    
+        case 3:
+            /* code */
+            break;
+    
+        case 4:
+            /* code */
+            break;
+    
+        case 5:
+            /* code */
+            break;
+    
+        case 6:
+            /* code */
+            break;
+    
+        default:
+            break;
+    }
+    
     return 0;
 }
 
