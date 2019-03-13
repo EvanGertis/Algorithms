64948e90d740827f82ad0f64866d2992716b0107
EvanGertis <39931491+EvanGertis@users.noreply.github.com>
2019-03-13 04:13:25 -0400

    addded implementations

--- C_Algorithms/queue_linkedlist/queue.c
+++ C_Algorithms/queue_linkedlist/queue.c
@@ -10,11 +10,10 @@ struct node *front, *rear;
 
 //Begin definitions.
 void initializeQueue();
-int isFull();
 int isEmpty();
 int size();
 void insert(int x);
-void Delete();
+int Delete();
 int peek();
 void display();
 //End definitions.
@@ -51,7 +50,7 @@ int main(){
                 insert(x);
                 break;
             case 2:
-                Delete();
+                printf("%d delete from the queue\n", Delete());
                 break;
         
             case 3:
@@ -86,48 +85,102 @@ void initializeQueue(){
 }
 //END initializeQueue.
 
-//BEGIN isFull.
-int isFull(){
-
-    return 0;
-}
-//END isFull.
-
 //BEGIN  isEmpty.
 int isEmpty(){
-
-    return 0;
+    if(front == NULL && rear == NULL){
+        return 1;
+    } else {
+        return 0;
+    }
 }
 //END isEmpty.
 
 //BEGIN size.
 int size(){
+    struct node *p;
+    int c;
 
-    return 0;
+    if(isEmpty()){
+        printf("List is empty\n");
+        return 0;
+    }
+    
+    p = front;
+    while(p != rear){
+        c++;
+        p= p->link;
+    }
+
+    return c;
 }
 //END size.
 
 //BEGIN insert.
 void insert(int x){
+    struct node *p, *temp;
+    temp = (struct node *)malloc(sizeof(struct node));
 
+    //gaurd.
+    if(temp == NULL){
+        printf("memory not avail");
+        return;
+    }
+
+    temp->info = x;
+    temp->link = NULL;
+
+    if(front == NULL){
+        front = temp;
+    }
+    else {
+        rear->link = temp;
+    }
+    rear = temp;
 }
 //END insert.
 
 //BEGIN Delete.
-void Delete(){
-
+int Delete(){
+    struct node *temp;
+    int x;
+    if(isEmpty()){
+        printf("list is empty\n");
+        exit(1);
+    }
+    temp = front;
+    x= temp->info;
+    front= front->link;
+    free(temp);
+    return x;
 }
 //END Delete.
 
 //BEGIN peek.
 int peek(){
+    if(isEmpty()){
+        printf("list is empty\n");
+        exit(1);
+    }
 
-    return 0;
+    return front->info;
 }
 //END peek.
 
 //BEGIN 
 void display(){
+    struct node *p;
+    
+    if(isEmpty()){
+        printf("list is empty\n");
+        return;
+    }
+
+    p = front;
+    while(p != rear){
+        printf("|%d| ", p->info);
+        p= p->link;
+    }
+    printf("\n");
 
 }
 //END
