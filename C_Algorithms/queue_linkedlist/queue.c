a39001863fd85c81e3f191953a6200a96261dba0
EvanGertis <39931491+EvanGertis@users.noreply.github.com>
2019-03-13 03:56:01 -0400

    added choice selection

--- C_Algorithms/queue_linkedlist/queue.c
+++ C_Algorithms/queue_linkedlist/queue.c
@@ -28,12 +28,47 @@ int main(){
 
     //UI loop.
     while(1){
-        printf("1.Insert an element into the queue.");
-        printf("2.Delete an element from the queue.");
-        printf("3.Peek the queue.");
-        printf("4.Display all.");
-        printf("5.Display size.");
-        printf("6.Quit.");
+        printf("1.Insert an element into the queue.\n");
+        printf("2.Delete an element from the queue.\n");
+        printf("3.Peek the queue.\n");
+        printf("4.Display all.\n");
+        printf("5.Display size.\n");
+        printf("6.Quit.\n");
+
+        //get selection.
+        scanf("%d", &choice);
+
+        if(choice == 6){
+            break;
+        }
+
+        //process selection.
+        switch (choice)
+        {
+            case 1:
+                printf("Please enter the value that you would like to insert\n");
+                scanf("%d", &x);
+                insert(x);
+                break;
+            case 2:
+                Delete();
+                break;
+        
+            case 3:
+                peek();
+                break;
+        
+            case 4:
+                display();
+                break;
+        
+            case 5:
+                printf("The size of the queue is %d", size());
+                break;
+        
+            default:
+                break;
+        }
     }
 
 
