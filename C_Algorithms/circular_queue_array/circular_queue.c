39c8b929d4f75598a740bcd93d9c2cbbfb7776e0
EvanGertis <39931491+EvanGertis@users.noreply.github.com>
2019-03-15 07:11:04 -0400

    added test code

--- C_Algorithms/circular_queue_array/circular_queue.c
+++ C_Algorithms/circular_queue_array/circular_queue.c
@@ -26,7 +26,60 @@ void display();
 // End definitions.
 
 int main(){
-    printf("Test");
+    int choice, x;
+
+    //set rear and front to -1.
+    initializeQueue();
+
+    // update UI.
+    while(1){
+        printf("1. Get size of queue.\n");
+        printf("2. Insert new element into the queue.\n");
+        printf("3. Delete.\n");
+        printf("4. Peek.\n");
+        printf("5. Display all items.\n");
+        printf("6. Exit\n");
+
+        //get input.
+        scanf("%d", choice);
+
+        //Exit program.
+        if(choice == 6){
+            break;
+        }
+
+        //decicion tree.
+        switch (choice)
+        {
+            case 1:
+                printf("The size of the queue is %d \n", size());
+                break;
+        
+            case 2:
+                printf("Please enter the element that you would like to insert into the queue\n");
+                scanf("%d", x);
+                insert(x);
+                break;
+        
+            case 3:
+                printf("The element deleted was %d \n", Delete());
+                break;
+        
+            case 4:
+                printf("The next value in the queue is %d \n", peek());
+                break;
+        
+            case 5:
+                display();
+                break;
+        
+            default:
+                break;
+        }
+
+    }
+
+    
     return 0;
 }
 
@@ -49,6 +102,7 @@ int size(){
 
 //Begin isFull
 int isFull(){
+    // case 1 front
 
     return 0;
 }
@@ -57,6 +111,9 @@ int isFull(){
 
 //Begin isEmpty
 int isEmpty(){
+    // case 1 front and rear = -1.
+    // case 2 rear = front - 1.
+    // case 3 front = 0 and rear = MAX -1
 
     return 0;
 }
