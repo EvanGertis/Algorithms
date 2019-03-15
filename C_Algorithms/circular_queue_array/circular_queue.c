d0ebf481e86e36c58ef3faed97787152f2d529f7
EvanGertis <39931491+EvanGertis@users.noreply.github.com>
2019-03-15 03:44:06 -0400

    cleaned up folders and made skeleton for array implementation

---
+++ C_Algorithms/circular_queue_array/circular_queue.c
@@ -0,0 +1,95 @@
+/*
+Name of file: circular_queue.c
+Purpose: Implement a circular queue using an array data structure.
+Author: Evan Gertis.
+Date: 03/15/2019.
+*/
+
+#include "stdlib.h"
+#include "stdio.h"
+#define MAX 5
+
+//globals.
+int a[MAX];
+int front, rear;
+
+
+// Begin defintions.
+void initializeQueue();
+int size();
+int isFull();
+int isEmpty();
+void insert(int x);
+int Delete();
+int peek();
+void display();
+// End definitions.
+
+int main(){
+    printf("Test");
+    return 0;
+}
+
+// Begin implementations.
+
+//Begin initializeQueue
+void initializeQueue(){
+
+}
+//End initializeQueue
+
+
+//Begin size
+int size(){
+
+    return 0;
+}
+//End size
+
+
+//Begin isFull
+int isFull(){
+
+    return 0;
+}
+//End isFull
+
+
+//Begin isEmpty
+int isEmpty(){
+
+    return 0;
+}
+//End isEmpty
+
+
+//Begin insert
+void insert(int x){
+
+}
+//End insert
+
+
+//Begin Delete
+int Delete(){
+
+    return 0;
+}
+//End Delete
+
+
+//Begin peek
+int peek(){
+
+    return 0;
+}
+//End peek
+
+
+//Begin display
+void display(){
+
+}
+//End display
+
+// End implementations.
