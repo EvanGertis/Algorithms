d7f4afc4e761ba10ae51c7495bc6ce35c52262f0
EvanGertis <39931491+EvanGertis@users.noreply.github.com>
2019-03-26 04:52:14 -0400

    added base code for program implementation

--- C_Algorithms/open_addressing/open_addr.c
+++ C_Algorithms/open_addressing/open_addr.c
@@ -8,8 +8,90 @@ Date: 03/26/2019
 #include <stdio.h>
 #include <stdlib.h>
 
+
+//Constants.
+
+//table size.
+#define TSIZE 19;
+
+//*********
+
+
+//TYPE DEFINTIONS
+
+//Used to keep track of record status.
+enum type_of_record {EMPTY, DELETED, OCCUPIED};
+
+//Stores student properties.
+struct student{
+
+    int studentId;
+    char studentName[20];
+
+};
+
+//Stores properties for student records.
+struct Record{
+
+    struct student_info;
+    enum type_of_record status;
+
+};
+
+//***************
+
+//BEGIN definitions
+void insert(struct student rec, struct Record table[]);
+int search(int key, struct Record table[]);
+void del(int key, struct Record table[]);
+void display(struct Record table[]);
+int hash(int key);
+//END definitions
+
 int main(){
 
     printf("Hello there");
     return 0;
 }
+
+
+//BEGIN Implimentations.
+
+//Used to insert a new record into the table.
+//BEGIN insert
+void insert(struct student rec, struct Record table[]){
+
+}
+//END insert
+
+//Used to find a record in table via key.
+//BEGIN search
+int search(int key, struct Record table[]){
+    
+    return 0;
+}
+//END search
+
+//Used to delete a record in the table via key.
+//BEGIN del
+void del(int key, struct Record table[]){
+
+}
+//END del
+
+//displays all records in the table.
+//BEGIN display
+void display(struct Record table[]){
+
+}
+//END display
+
+//Hashing function used to generate keys.
+//BEGIN hash
+int hash(int key){
+
+    return 0;
+}
+//END hash
+
+//END Implementations.
