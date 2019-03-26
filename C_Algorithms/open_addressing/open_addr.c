080e13909adfd44c9673ba58bb11cbf93d23157e
EvanGertis <39931491+EvanGertis@users.noreply.github.com>
2019-03-26 04:40:03 -0400

    added files for open addr problem

---
+++ C_Algorithms/open_addressing/open_addr.c
@@ -0,0 +1,15 @@
+/*
+File Name: open_addr.c
+Author: Evan Gertis.
+Purpose: implement the open addressing technique for hashing.
+Date: 03/26/2019
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    printf("Hello there");
+    return 0;
+}
