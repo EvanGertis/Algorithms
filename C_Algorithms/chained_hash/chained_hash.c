3e3cf3caffd42571b431f38016ca7e30e5e97ef4
EvanGertis <39931491+EvanGertis@users.noreply.github.com>
2019-03-28 06:52:32 -0400

    added makefiles and test program

---
+++ C_Algorithms/chained_hash/chained_hash.c
@@ -0,0 +1,16 @@
+/*
+File Name: chained_hash.c
+Author : Evan Gertis
+Purpose : contains implementation of chained hash table.
+Date : 03/28/2019
+*/
+
+#include "stdio.h"
+#include "stdlib.h"
+
+int main(){
+
+    printf("Hello");
+
+    return 0;
+}
