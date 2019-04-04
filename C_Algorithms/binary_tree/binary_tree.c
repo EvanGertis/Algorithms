f8953aad76b43c83695447a4dce985393f229686
EvanGertis <39931491+EvanGertis@users.noreply.github.com>
2019-04-04 05:29:11 -0400

    tests pass

--- C_Algorithms/binary_tree/binary_tree.c
+++ C_Algorithms/binary_tree/binary_tree.c
@@ -43,7 +43,38 @@ int queueEmpty();
 //************************************
 //************************************
 
+//main entry point of the program.
 int main(){
+    struct node *root = NULL;
+
+    root = getnode('P');
+    root->lchild = getnode('Q');
+    root->rchild = getnode('R');
+    root->lchild->lchild = getnode('A');
+    root->lchild->rchild = getnode('B');
+    root->rchild->lchild = getnode('X');
+
+    display(root, 0);
+    printf("\n\n");
+
+    printf("Preorder : ");
+    preorder(root);
+    printf("\n\n");
+
+    printf("Inorder : ");
+    inorder(root);
+    printf("\n\n");
+
+    printf("Postorder : ");
+    postorder(root);
+    printf("\n\n");
+
+    printf("Level order : ");
+    levelOrder(root);
+    printf("\n\n");
+
+    printf("Height of tree is %d\n", height((root)));
+
     return 0;
 }
 
@@ -54,61 +85,145 @@ int main(){
 
 //allocates new node for binary tree.
 struct node *getnode(char x){
-    struct node *rm;
+    struct node *p = (struct node *)malloc(sizeof(struct node));
+    p->info = x;
+    p->lchild = p->rchild = NULL;
 
-    return rm;
+    return p;
 }//end getnode
 
 //displays the binary tree.
 void display(struct node *p, int level){
 
+    if(p == NULL){
+        return;
+    }
+
+    display(p->rchild, level + 1);
+    printf("\n");
+
+    for(int i = 0; i < level; i++){
+        printf("   ");
+        printf("%c", p->info);
+    }
+
+    display(p->lchild, level + 1);
+
 }//end display
 
 //binary tree traversal method.
 void preorder(struct node *p){
+    if(p == NULL){
+        return;
+    }
+    printf("%c ", p->info);
+    preorder(p->lchild);
+    preorder(p->rchild);
 
 }//end preorder
 
 //binary tree traversal method.
 void inorder(struct node *p){
 
+    if(p==NULL){
+        return;
+    }
+
+    inorder(p->lchild);
+    printf("%c ", p->info);
+    inorder(p->rchild);
+
 }//end inorder
 
 //binary tree traversal method.
 void postorder(struct node *p){
+    if(p==NULL){
+        return;
+    }
+
+    postorder(p->lchild);
+    postorder(p->rchild);
+    printf("%c ", p->info);
 
 }//end postorder
 
 //binary tree traversal method.
 void levelOrder(struct node *p){
+    if(p == NULL){
+        printf("Tree is empty\n");
+        return;
+    }
 
+    insertQueue(p);
+    while(!queueEmpty()){
+        p = deleteQueue();
+        printf("%c ", p->info);
+        if(p->lchild != NULL){
+            insertQueue(p->lchild);
+        }
+        if(p->rchild != NULL){
+            insertQueue(p->rchild);
+        }
+    }
 }//end levelOrder
 
 //returns the height of the binary tree.
 int height(struct node *p){
+    int hL, hR;
 
-    return 0;
+    if(p == NULL){
+        return 0;
+    }
+
+    hL = height(p->lchild);
+    hR = height(p->rchild);
+
+    if(hL > hR){
+        return 1 + hL;
+    } 
+    else{
+        return 1 + hR;
+    }
 }//end height
 
 //utility**************
 
 //adds an item to the queue.
 void insertQueue(struct node *item){
-
+    if(rear == MAX -1){
+        printf("Queue Overflow\n");
+        return;
+    }
+    if(front == -1){
+        front = 0;
+    }
+    rear = rear + 1;
+    queue[rear] = item;
 }
 
 //deletes an item from the queue.
-struct node * deleteQueue(){
+struct node *deleteQueue(){
+    struct node *item;
+    if(front == -1 || front == rear + 1){
+        printf("Queue Underflow\n");
+        return 0;
+    }
 
-    struct node *rm;
-
-    return rm;
+    item = queue[front];
+    front = front + 1;
+    return item;
 }
 
 //returns 1 if the queue is empty
 int queueEmpty(){
 
-    return 1;
+    if(front == -1 || front == rear + 1){
+        return 1;
+    }
+    else{
+
+        return 0;
+    }
 }
 
 
