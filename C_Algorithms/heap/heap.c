58f4f64ae55f249ead218d10895b664628e76a44
EvanGertis <39931491+EvanGertis@users.noreply.github.com>
2019-04-10 08:03:13 -0400

    added base files, defs, and implementations

---
+++ C_Algorithms/heap/heap.c
@@ -0,0 +1,49 @@
+/*
+Author: Evan Gertis
+File Name: heap.c
+Purpose: Implementation of heap tree.
+Date: 04/10/2019
+*/
+
+#include <stdio.h>
+#define LARGE_VALUE 99999
+
+void insert(int value, int a[], int *pn);
+int deleteRoot(int a[], int *pn);
+void restoreUp(int a[], int loc);
+void restoreDown(int a[], int i, int size);
+void display(int a[], int n);
+
+int main(){
+
+    return 0;
+}
+
+
+//begin insert
+void insert(int value, int a[], int *pn){
+
+}
+//end insert
+
+//begin deleteRoot
+int deleteRoot(int a[], int *pn){
+
+}
+//end deleteRoot
+
+//begin restoreUp
+void restoreUp(int a[], int loc){
+
+}
+//end restoreUp
+
+//begin restoreDown
+void restoreDown(int a[], int i, int size){
+}
+//end restoreDown
+
+//begin display
+void display(int a[], int n){
+}
+//end display
