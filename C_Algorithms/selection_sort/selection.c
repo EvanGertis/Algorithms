c28c70f3b61578a6aae72d639cf15d8355642e87
EvanGertis <39931491+EvanGertis@users.noreply.github.com>
2019-04-16 20:00:00 -0400

    selection sort added

--- C_Algorithms/selection_sort/selection.c
+++ C_Algorithms/selection_sort/selection.c
@@ -1,36 +1,39 @@
 #include "stdlib.h"
 #include "stdio.h"
 
-#define SIZE 5
-
-int a[SIZE] = {2, 6, 3, 1, 5};
-
-void displayArray(int a[]);
+#define MAX 100
 
 int main(){
-    
-    displayArray(a);
 
-    for(int i = 0; i < SIZE; i++){
-        for(int j = 1; j < SIZE - i; j++){
-            if(a[j-1] > a[j]){
-                int temp;
-                temp = a[j];
-                a[j] = a[j-1];
-                a[j-1] = temp;
+    int a[MAX], n, temp, minIndex;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++){
+        printf("Enter element %d:", i);
+        scanf("%d", &a[i]);
+    }
+
+    for(int i = 0; i < n - 1; i++){
+
+        minIndex = i;
+        for(int j = i + 1; j < n; j++){
+            if(a[j] < a[minIndex]){
+                minIndex = j;
             }
+        }
+
+        if(i != minIndex){
+            temp = a[i];
+            a[i] = a[minIndex];
+            a[minIndex] = temp; 
         }
     }
 
-    displayArray(a);
-
-    return 0;
-}
-
-void displayArray(int a[]){
-
-    for(int i = 0; i < SIZE; i++){
-        printf("%d | ", a[i]);
+    printf("Sorted array is : \n");
+    for(int i = 0; i < n; i++){
+        printf("%d ", a[i]);
     }
     printf("\n");
 
