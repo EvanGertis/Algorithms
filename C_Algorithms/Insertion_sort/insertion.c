a44445e39142c8f9fb119c1fe3b06820043d9146
EvanGertis <39931491+EvanGertis@users.noreply.github.com>
2019-04-18 05:12:16 -0400

    added insertion sort

--- C_Algorithms/Insertion_sort/insertion.c
+++ C_Algorithms/Insertion_sort/insertion.c
@@ -1,10 +1,42 @@
 #include "stdio.h"
 #include "stdlib.h"
 
-
+#define MAX 100
 
 
 int main(){    
 
+    int a[MAX], i, j, temp, n;
+
+    printf("Enter the number of elements : ");
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++){
+        printf("Enter the element %d : ", i);
+        scanf("%d", &a[i]);
+    }
+
+    for(int i = 0; i < n; i++){
+        printf("%d | ", a[i]);
+    }
+
+    printf("\n");
+
+    for(i = 1; i < n; i++){
+        temp = a[i];
+
+        for(j = i - 1; j >=0 && a[j] > temp; j--){
+            a[j+1] = a[j];
+        }
+
+        a[j+1] = temp;
+    }
+
+    for(int i = 0; i < n; i++){
+        printf("%d | ", a[i]);
+    }
+
+    printf("\n");
+    
     return 0;
 }
